eab9a3f3a28a9276bc9cf3025cf2b0b8d0595af6
robokae <alexanderhom19@gmail.com>
2021-11-15 23:11:54 -0800

    implemented changing tables using ajax

--- app.py
+++ app.py
@@ -1,11 +1,78 @@
 # Main application code
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
-# @app.route("/")
-# def base():
-#     return render_template("base.html")
+sample_enrolled_data = [
+    {
+        'name': 'BIO 1',
+        'instructor': 'John Smith',
+        'time': 'MWF 11:00am - 12:15pm',
+        'enrollment': '65/200'
+    },
+    {
+        'name': 'PHYS 10',
+        'instructor': 'Jane Doe',
+        'time': 'TR 5:00pm - 6:15pm',
+        'enrollment': '92/150'
+    }
+]
+
+sample_courses_data = [
+    {
+        'option': 'Remove',
+        'name': 'BIO 1',
+        'instructor': 'John Smith',
+        'time': 'MWF 11:00am - 12:15pm',
+        'enrollment': '65/200'
+    },
+    {
+        'option': 'Add',
+        'name': 'BIO 10',
+        'instructor': 'John Smith',
+        'time': 'TR 12:00pm - 1:15pm',
+        'enrollment': '82/150'
+    },
+    {
+        'option': 'Add',
+        'name': 'BIO 122',
+        'instructor': 'John Smith',
+        'time': 'MW 2:00pm - 3:15pm',
+        'enrollment': '102/120'
+    },
+    {
+        'option': 'Remove',
+        'name': 'PHYS 10',
+        'instructor': 'Jane Doe',
+        'time': 'TR 5:00pm - 6:15pm',
+        'enrollment': '92/150'
+    }
+]
+
+sample_instructor_data = [
+    {
+        'name': 'BIO 1',
+        'instructor': 'John Smith',
+        'time': 'MWF 11:00am - 11:50am',
+        'enrollment': '65/200'
+    },
+    {
+        'name': 'BIO 10',
+        'instructor': 'John Smith',
+        'time': 'TR 12:00pm - 1:15pm',
+        'enrollment': '82/150'
+    },
+    {
+        'name': 'BIO 122',
+        'instructor': 'John Smith',
+        'time': 'MW 2:00pm - 3:15pm',
+        'enrollment': '102/120'
+    }
+]
 
 @app.route("/")
 def login():
@@ -13,27 +80,22 @@ def login():
 
 @app.route("/student/<name>")
 def student(name):
-    sample_data = [
-        {
-            'name': 'BIO 1',
-            'instructor': 'John Smith',
-            'time': 'MWF 11:00am - 12:15pm',
-            'enrollment': '65/200'
-        },
-        {
-            'name': 'PHYS 10',
-            'instructor': 'Jane Doe',
-            'time': 'TR 5:00pm - 6:15pm',
-            'enrollment': '92/150'
-        }
-    ]
     student_name = name
-    return render_template("student.html", student_name = student_name, data = sample_data)
+    return render_template("student.html", student_name = student_name)
 
 @app.route("/instructor/<name>")
 def instructor(name):
     instructor_name = name
     return render_template("instructor.html", instructor_name = instructor_name)
+
+@app.route("/enrolled/<name>")
+def enrolled(name):
+    # return array of json
+    return jsonify(sample_enrolled_data)
+
+@app.route("/courses")
+def courses():
+    return jsonify(sample_courses_data)
 
 @app.route("/index")
 def index():
--- static/main.js
+++ static/main.js
@@ -1,1 +1,107 @@
-// JS
+let xhttp = new XMLHttpRequest();
+let method;
+let baseURL = "http://localhost:5000";
+let async = true;
+
+const tableHeadings = {
+    enrolledTableHeadings: ["Name", "Instructor", "Time", "Enrollment"],
+    addTableHeadings: ["Option", "Name", "Instructor", "Time", "Enrollment"],
+    instructorCoursesHeadings: ["Name", "Instructor", "Time", "Enrollment"],
+    courseGradebookHeadings: ["Student", "Grade"]
+}
+
+function generateHTML(tableData, headings) {
+    let headingsHTML = "";
+    let tableBodyHTML = "";
+    let tableRowLength;
+
+    for (let i = 0; i < headings.length; i++) {
+        headingsHTML += `<th>${headings[i]}</th>`;
+    }
+
+    for (let j = 0; j < tableData.length; j++) {
+        tableBodyHTML += "<tr>"
+
+        tableRowLength = Object.keys(tableData[j]).length;
+
+        for (let k = 0; k < tableRowLength; k++) {
+            tableBodyHTML += `<td>${tableData[j][headings[k].toLowerCase()]}</td>`;
+        }
+
+        tableBodyHTML += "</tr>";
+    }
+
+    return {
+        headingsHTML,
+        tableBodyHTML
+    };
+}
+
+function generateTable(headingType, dataFromServer) {
+    let headingsRow = document.querySelector(".headings-row");
+    let tableBody = document.querySelector(".data-table-body");
+
+    let tableData = JSON.parse(dataFromServer);
+
+    switch (headingType) {
+        case ("enrolled"):
+            tableHTML = generateHTML(tableData, tableHeadings.enrolledTableHeadings);
+            break;
+        case ("add"):
+            tableHTML = generateHTML(tableData, tableHeadings.addTableHeadings);
+            break;
+    }
+
+    headingsRow.innerHTML = tableHTML.headingsHTML;
+    tableBody.innerHTML = tableHTML.tableBodyHTML;
+}
+
+
+function getTable(nameOfTab, studentName) {
+    let enrolledTab = document.querySelector(".enrolled-tab");
+    let addTab = document.querySelector(".add-tab");
+
+    // get enrolled table 
+    if (nameOfTab === "enrolled") {
+        // change tab styling
+        if (!enrolledTab.classList.contains("active-tab")) {
+            enrolledTab.classList.add("active-tab");
+        }
+        
+        if (addTab.classList.contains("active-tab")) {
+            addTab.classList.remove("active-tab");
+        }
+
+        method = "GET";
+        let url = `${baseURL}/enrolled/${studentName}`;
+        xhttp.open(method, url, async);
+
+        xhttp.onload = function() {
+            generateTable(nameOfTab, this.response);
+        };
+        
+        xhttp.send();
+    }
+
+    // get add table
+    else if (nameOfTab === "add") {
+        // change tab styling
+        if (!addTab.classList.contains("active-tab")) {
+            addTab.classList.add("active-tab");
+        }
+
+        if (enrolledTab.classList.contains("active-tab")) {
+            enrolledTab.classList.remove("active-tab");
+        }
+
+        method = "GET";
+        url = `${baseURL}/courses`;
+        xhttp.open(method, url, async);
+
+        xhttp.onload = function() {
+            generateTable(nameOfTab, this.response);
+        };
+
+        xhttp.send();
+    }
+}
