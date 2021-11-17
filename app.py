7951b77f739738ba9e3955e929c095acd43f77f3
Christian-John Forcadilla <cjforcadilla00@gmail.com>
2021-11-16 19:48:45 -0800

    implemented flask admin + partial flask login

--- app.py
+++ app.py
@@ -1,22 +1,103 @@
 # Main application code
 from flask import Flask, render_template, request, url_for, redirect
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from flask_login import LoginManager
+from db import User, Teacher, Student, Course, Enrollment, db
+
+# from flask_admin import Admin, AdminIndexView
+# from flask_login import LoginManager, current_user, login_user, logout_user
+# from flask_login.utils import login_required
+# from flask_sqlalchemy.model import Model
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# -------- Admin ----------
+admin = Admin(app)
+
+app.config['FLASK_ADMIN_SWATCH'] = 'Cyborg'
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Teacher, db.session))
+admin.add_view(ModelView(Student, db.session))
+admin.add_view(ModelView(Course, db.session))
+admin.add_view(ModelView(Enrollment, db.session))
+
+# -------- Login ----------
+# LoginManager = instance of login
+
+# login = LoginManager()
+# login.__init__app(app)
+# login.login_view = 'login'
+
+# app.config['SECRET_KEY'] = 'testkey'
+
+# @login.user_loader
+# def load_user(user_id)for:
+#     return User.query.get(int(user_id))
+# -------------------------
 
 # @app.route("/")
 # def base():
 #     return render_template("base.html")
 
+# -------- Admin ----------
+# class UserView(ModelView):
+#     def is_accessible(self):
+#         return True
+
+# class TeacherView(ModelView):
+#     def is_accessible(self):
+#         return True
+
+# class StudentView(ModelView):
+#     def is_accessible(self):
+#         return True
+
+# class CourseView(ModelView):
+#     def is_accessible(self):
+#         return True
+
+# class EnrollmentView(ModelView):
+#     def is_accessible(self):
+#         return True
+
+# --------------------------
+
+
 @app.route("/", methods = ["POST", "GET"])
 def login():
+
+    # if current_user.is_authenticated:
+    #     return redirect(url_for('index'))
+
     if request.method == "POST":
-        user = request.form["username"]
+        username = request.form["username"]
         password = request.form["password"]
-        return redirect(url_for("student", name = user))
+    
+        # checkUser = User.query.filter_by(username=username).first()
+        
+
+        # if not checkUser or not checkUser.check_password(password):
+        #         return render_template("login.html")
+
+    #     login_user(user)
+
+        return redirect(url_for("student", name = username))
+
     else:
         return render_template("login.html")
 
+
+# @app.route('/logout')
+# @login_required
+# def logout():
+#     logout_user
+
+
 @app.route("/student/<name>")
+# @login_required
 def student(name):
     sample_data = [
         {
@@ -36,17 +117,18 @@ def student(name):
     return render_template("student.html", student_name = student_name, data = sample_data)
 
 @app.route("/instructor/<name>")
+# @login_required
 def instructor(name):
     instructor_name = name
     return render_template("instructor.html", instructor_name = instructor_name)
 
 @app.route("/index")
 def index():
-    return render_template("grade/index.html")
+    return render_template("index.html")
 
 @app.route("/update")
 def update():
-    return render_template("grade/update.html")
+    return render_template("update.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- db.py
+++ db.py
@@ -1,12 +1,16 @@
 # Database code
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+# from flask_login import UserMixin
+
+# UserMixin = methods that are needed for flask login
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
+# class User(UserMixin, db.Model):
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(15), unique=True, nullable=False)
