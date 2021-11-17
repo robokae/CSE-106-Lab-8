4ea1a6fc3ded2816b87858101222d43b4184e524
jluna2000 <jesseluna2000@gmail.com>
2021-11-16 21:34:18 -0800

    Database Implementation to show all courses when adding courses for Student

--- app.py
+++ app.py
@@ -19,20 +19,6 @@ def login():
 
 @app.route("/student/<name>")
 def student(name):
-    # sample_data = [
-    #     {
-    #         'name': 'BIO 1',
-    #         'instructor': 'John Smith',
-    #         'time': 'MWF 11:00am - 12:15pm',
-    #         'enrollment': '65/200'
-    #     },
-    #     {
-    #         'name': 'PHYS 10',
-    #         'instructor': 'Jane Doe',
-    #         'time': 'TR 5:00pm - 6:15pm',
-    #         'enrollment': '92/150'
-    #     }
-    # ]
     student_name = name
     try_data = []
     stuQ = Student.query.filter_by(name=student_name).first()
@@ -45,8 +31,19 @@ def student(name):
             'enrollment':(str(stuQC[i].course.number_enrolled) + "/" + str(stuQC[i].course.capacity))
         }
         try_data.append(temp)
+
+    # CODE TO DISPLAY ALL COURSES 
+    # courseQ = Course.query.all()
+    # for i in range(len(courseQ)):
+    #     temp = {
+    #         'name':courseQ[i].course_name,
+    #         'instructor':courseQ[i].teacher.name,
+    #         'time':courseQ[i].time,
+    #         'enrollment':(str(courseQ[i].number_enrolled) + "/" + str(courseQ[i].capacity))
+    #     }
+    #     try_data.append(temp)
+    # # CODE TO DISPLAY ALL COURSES (END)
     
-    # return render_template("student.html", student_name = student_name, data = sample_data)
     return render_template("student.html", student_name = student_name, data = try_data)
 
 @app.route("/instructor/<name>")
@@ -63,6 +60,7 @@ def instructor(name):
             'enrollment':(str(teaQC[i].number_enrolled) + "/" + str(teaQC[i].capacity))
         }
         tea_data.append(temp)
+    # CODE TO DISPLAY TABLE OF STUDENTS AND THEIR GRADES BASED OFF OF THE SELECTED CLASS
     # tea_courses_data = {}
     # for i in range(len(teaQC)):
     #   courseT = []
@@ -74,15 +72,18 @@ def instructor(name):
     #       courseT.append(temp1)
     #   tea_courses_data[teaQC[i].course_name] = courseT
     # returnData = tea_course_data[courseName] <- this line of code would work off of the course clicked by the teacher, returning the table of students and grades pertaining to the course labeled courseName
+    # CODE TO DISPLAY TABLE OF STUDENTS AND THEIR GRADES BASED OFF OF THE SELECTED CLASS (END)
     return render_template("instructor.html", instructor_name = instructor_name, data = tea_data)
 
-@app.route("/index")
-def index():
-    return render_template("grade/index.html")
+# USELESS CODE THUS FAR
+# @app.route("/index")
+# def index():
+#     return render_template("grade/index.html")
 
-@app.route("/update")
-def update():
-    return render_template("grade/update.html")
+# @app.route("/update")
+# def update():
+#     return render_template("grade/update.html")
+# USELESS CODE THUS FAR (END)
 
 if __name__ == "__main__":
     app.run()
